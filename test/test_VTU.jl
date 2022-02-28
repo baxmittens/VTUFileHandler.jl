68115ff92adb24ac6b53a3c086c1b557c8b9f08a
Bittens <Bittens.M@bgr.local>
2022-02-28 12:22:52 +0100

    upd

--- test/test_VTU.jl
+++ test/test_VTU.jl
@@ -1,18 +1,4 @@
-include(joinpath("..","src","Ogs6InputFileHandler.jl"))
-include(joinpath("..","src","VTUFileHandler.jl"))
-
-file = joinpath(".","Point_injection","square_1e2_lin.prj")
-ogs6md = read(Ogs6ModelDef, file)
-write(ogs6md)
-
-file1 = joinpath(".","Point_injection","expected_square_1e0_ts_10_t_1000.000000.vtu")
-file2 = joinpath(".","Point_injection","expected_square_1e0_lin_ts_10_t_50000.000000.vtu")
-file3 = joinpath(".","Point_injection","expected_square_1e0_ts_10_t_50000.000000.vtu")
-
-vtu1 = VTUFile(file1);
-vtu2 = VTUFile(file2);
-vtu3 = VTUFile(file3);
-
-write(vtu1)
-
+file = joinpath(".","pointheatsource.vtu")
+vtu1 = VTUFileHandler.VTUFile(file);
 tmp = similar(vtu1.data)
+vtu += tmp
