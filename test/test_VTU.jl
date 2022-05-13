6661da878bde30591170b1dfe4bacb9e45b8b1c1
Bittens <Bittens.M@bgr.local>
2022-05-13 10:29:48 +0200

    ins

--- test/test_VTU.jl
+++ test/test_VTU.jl
@@ -1,5 +1,15 @@
-file = joinpath(".","pointheatsource.vtu")
-vtu = VTUFileHandler.VTUFile(file);
-tmp = similar(vtu)
-vtu += tmp
-true
+using VTUFileHandler
+using Test
+
+@testset "VTUFileHandler test" begin
+    file = joinpath(".","pointheatsource.vtu")
+    tempfieldstr = "temperature_interpolated"
+    set_interpolation_keywords([tempfieldstr])
+	vtu = VTUFileHandler.VTUFile(file);
+	tmp = zero(vtu)
+	tmp += vtu
+	vtu += tmp
+    @test all(vtu[tempfieldstr] .== 2.0*tmp[tempfieldstr])
+    vtu -= tmp
+    @test all(vtu[tempfieldstr] .== tmp[tempfieldstr])
+end 
